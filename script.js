1dc5788b8ad83105e5a45e21ed5451a62488e432
iamshaylajade <shaylajade21@yahoo.com>
2023-10-12 16:34:58 -0400

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var characters = "!@#$%&*[]";
+
+var generateBtn = document.getElementById("generate");
+function generatePassword() {
+
+var passwordLength = prompt("How many characters?");
+var hasUppercase = prompt ("Would you like to use capital letters?")
+var hasLowercase = prompt ("Would you like to use Lowercase letters?")
+var specialcharacters = prompt ("Would you like to use special characters")
+
+console.log(passwordLength)
+console.log("Welcome to generate password");
+return "Type New Password";
+
+// Prompt the user for the password criteria
+// Password length at least 8 characters
+// Lowercase, uppercase, numbers, special characters
+// Validate the input.
+// Generte password based on criteria
+// display password 
+
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
